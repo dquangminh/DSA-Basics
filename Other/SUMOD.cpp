f6b2365d309e159453747fff8d879ed8b16d7bb2
Duong Quang Minh <dquangminh1310@gmail.com>
2018-06-09 11:54:22 +0700

    Sum Mod

---
+++ Other/SUMOD.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define MOD 1000000007
+
+int addmod(int a, int b)
+{
+    a %= MOD;
+    b %= MOD;
+    int d = MOD - b;
+    return a < d? a+b: a-d;
+}
+
+int main()
+{
+    int S = 0, N, x;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &x);
+        S = addmod(S, x);
+    }
+    printf("%d\n", S);
+}
